dd9e426ab7cc3000184b707f97b4b3d403291c76
Jacob <jaysansa@gmail.com>
2019-02-19 13:18:29 +0100

    test using APIClient

--- pollapi/polls/tests.py
+++ pollapi/polls/tests.py
@@ -1,5 +1,5 @@
 from rest_framework.test import APITestCase
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
 
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
@@ -8,8 +8,8 @@ from polls import apiviews
 
 class TestPoll(APITestCase):
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.view = apiviews.PollViewSet.as_view({'get': 'list'})
+        self.client = APIClient()
+        # self.view = apiviews.PollViewSet.as_view({'get': 'list'})
         self.uri = '/polls/'
 
         self.user = self.setup_user()
@@ -26,9 +26,21 @@ class TestPoll(APITestCase):
         )
 
     def test_list(self):
-        request = self.factory.get(self.uri, HTTT_AUTHORIZATION='Token {}'.format(self.token.key))
-        request.user = self.user
-        response = self.view(request)
+        self.client.login(username='test', password='test')
+        response = self.client.get(self.uri)
+        # request.user = self.user
+        # # response = self.view(request)
         self.assertEqual(response.status_code, 200, 
                         'Expected Response Code 200, received {0} instead'
+                        .format(response.status_code))
+
+    def test_create(self):
+        self.client.login(username='test', password='test')
+        params = {
+            "question": "Where are you?",
+            "created_by": 1
+        }
+        response = self.client.post(self.uri, params)
+        self.assertEqual(response.status_code, 201, 
+                        'Expected Response Code 201, received {0} instead'
                         .format(response.status_code))
